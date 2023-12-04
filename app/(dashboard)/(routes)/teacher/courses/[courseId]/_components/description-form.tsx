4a162d086566b1274a5229dc2f3caafb177bdbfd
mikiyas <mikiyasget1234@gmail.com>
2023-12-04 11:40:01 +0000

    add description form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { Course } from "@prisma/client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+
+interface DescriptionFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  description: z.string().min(3, {
+    message: " is requierd",
+  }),
+});
+
+export const DescriptionForm = ({
+  initialData,
+  courseId,
+}: DescriptionFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const onToggleEdit = () => setIsEditing((current) => !current);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: initialData?.description || "",
+    },
+  });
+
+  const { isSubmitting } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("course updated");
+      onToggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("something went wrong");
+    }
+    console.log(values);
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course description
+        <Button variant="ghost" onClick={onToggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit description
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            "text-sm mt-2 ",
+            !initialData.description && "text-slate-500 italic"
+          )}
+        >
+          {initialData?.description || "No description"}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      disabled={isSubmitting}
+                      placeholder="e.g this is course description"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
