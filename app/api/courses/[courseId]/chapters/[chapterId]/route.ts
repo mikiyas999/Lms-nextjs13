9daa53941f36c4655f920448ca5cca8218438b8c
mikiyas <mikiyasget1234@gmail.com>
2023-12-15 09:08:18 +0000

    Mux integration

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { NextURL } from "next/dist/server/web/next-url";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
 
 export async function PATCH(
   req: Request,
@@ -10,7 +15,7 @@ export async function PATCH(
   try {
     const { userId } = auth();
 
-    const { isPublished, ...value } = await req.json();
+    const { isPublished, ...values } = await req.json();
 
     if (!userId) {
       return new NextResponse("UnAuthorized", { status: 401 });
@@ -32,10 +37,39 @@ export async function PATCH(
         id: params.chapterId,
       },
       data: {
-        ...value,
+        ...values,
       },
     });
 
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: "public",
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
     return NextResponse.json(chapter);
   } catch (error) {
     console.log("CHAPTER_ID_UPDATED", error);
