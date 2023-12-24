47a1195078ef989301cd8f6e2570a2712771dd01
mikiyas <mikiyasget1234@gmail.com>
2023-12-24 10:08:13 +0000

    add confetti to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/provider/toast-provider";
+import { ConfettiProvider } from "@/components/provider/confetti-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {children}
           <ToastProvider />
+          <ConfettiProvider />
+          {children}
         </body>
       </html>
     </ClerkProvider>
