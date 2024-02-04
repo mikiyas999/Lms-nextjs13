0d3d61c32c6240cddbb2159de50f75d2ee056968
mikiyas <mikiyasget1234@gmail.com>
2024-02-04 18:09:01 +0000

    update array iteration

--- app/(dashboard)/(routes)/(root)/loading.tsx
+++ app/(dashboard)/(routes)/(root)/loading.tsx
@@ -10,7 +10,7 @@ const PageLoading = () => {
         <InfoCardSekeleton />
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-8">
-        {[1, 2, 3, 4, 5].map((_, i) => (
+        {[...Array(8)].map((_, i) => (
           <CourseSkeletonCard key={i} />
         ))}
       </div>
