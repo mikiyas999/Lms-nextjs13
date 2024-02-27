01630e9bb596760c30f06df3082e6afb55747dd3
mikiyas <mikiyasget1234@gmail.com>
2024-02-27 18:09:31 +0000

    final

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,14 +1,15 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { isTeacher } from "@/lib/teacher";
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const { title } = await req.json();
 
-    if (!userId) {
-      return new Response("Unauthorized", { status: 401 });
+    if (!userId || !isTeacher(userId)) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const course = await db.course.create({
